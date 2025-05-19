666c5110e66ac9f95d4f5d90524cf81af0e9b847
ting0602 <ting0602.cs13@nycu.edu.tw>
2025-05-19 23:57:36 +0800

    fix: vitest.config.ts && debug info in ci.yaml

--- backend/vitest.config.ts
+++ backend/vitest.config.ts
@@ -2,6 +2,10 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
-    pool: 'forks'
+    pool: 'forks',
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'json', 'html']
+    }
   }
 })
